2744106cf23f1556712023bd08fa2d2941e95518
royce.com@gmail.com <royce.com@gmail.com@3adfe074-7735-e7d1-17c2-4101be38d09b>
2015-03-06 01:55:30 +0000

    Unit test for getting the correct combination.

--- main/src/test/java/junitcast/ListMergerTest.java
+++ main/src/test/java/junitcast/ListMergerTest.java
@@ -1,173 +1,61 @@
 /**
+ * Copyright 2014 Asian Development Bank. All rights reserved.
+ * ADB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
+ * Project Name:  ISTSII ADB Manage Procurement Review System.
+ * Module :  Monitoring
+ * Use Case:
+ * Purpose:
+ * Design Document Reference:  <full path where the design document is and page no>
+ * File Path: $HeadURL$
+ *
+ * Oracle ERP Release: 12.1.3
+ * Oracle ERP Module: Custom.
+ *
+ * Created by: Royce Remulla (R39)
  */
 package junitcast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 /**
- * Test class for SetMerger.
- *
- * <pre>
- * $Date$
- * $HeadURL$
- * </pre>
- *
+ * 
  * @author Royce Remulla
+ * @version $Date$
  */
-public class ListMergerTest
-        extends
-        AbstractTransientValueTestCase<ListMerger<String>, String, List<List<String>>> {
+public class ListMergerTest {
 
 
     /** */
-    public enum Variable {
-        /** */
-        list_null, empty_list, list_1x3, list_w_null, list_1x3_w_null,
+    private final transient ListMerger<String> sut = new ListMerger<String>();
 
-        /** */
-        list_1x0, list_3x1, list_2x3, list_3x5, list_2x5x3
-    }
-
-    /** @param pParameter Data Transfer Object Parameter in Parameterized test. */
-    public ListMergerTest(final Parameter<String> pParameter) {
-        super(pParameter);
-    }
 
     /**
-     * <pre>
-     * Test data generator.
-     * This method is called by the JUnit parameterized test runner and
-     * returns a Collection of Arrays.  For each Array in the Collection,
-     * each array element corresponds to a parameter in the constructor.
-     * </pre>
+     * Test basic scenario.
      */
-    @Parameters(name = "{0}")
-    public static Collection<Object[]> generateData()
+    @Test
+    public void simpleTest()
     {
-        return new ParameterGenerator<String>()
-            .genVarData("junitcast.ListMergerTest");
-    }
+        final List<String> list = new ArrayList<String>(
+            Arrays.asList(new String[] {
+                    "false",
+                    "true" }));
 
-    /** {@inheritDoc} */
-    @Override
-    protected void setupTargetObject(final List<Object> constructorParams)
-    {
-        new MockitoHelper().setupTargetObject(this, constructorParams);
-    }
+        final List<List<String>> all = new ArrayList<List<String>>();
+        all.add(list);
+        all.add(list);
 
-    /** {@inheritDoc} */
-    @Override
-    protected void prepare()
-    {
-        final ScenarioSource<String> source = new ScenarioSource<String>(this);
+        final List<List<String>> result = sut.merge(all);
 
-        source.addTransientCase(
-            0,
-            new ArrayList<Set<String>>(),
-            Variable.empty_list);
-
-        source.addTransientCase(
-            0,
-            new ArrayList<Set<String>>(),
-            Variable.list_w_null);
-
-        source.addObserver(Variable.list_w_null, new CaseObserver<String>() {
-            @Override
-            public void prepareCase(final String caseRaw)
-            {
-                final List<List<String>> listWithNull = new ArrayList<List<String>>();
-                listWithNull.add(createList(1));
-                listWithNull.add(null);
-                setTransientValue(0, listWithNull);
-            }
-        });
-
-        source.addObserver(
-            Variable.list_1x3_w_null,
-            new CaseObserver<String>() {
-                @Override
-                public void prepareCase(final String caseRaw)
-                {
-                    final List<List<String>> setWithNull = new ArrayList<List<String>>();
-                    setWithNull.add(createList(1));
-                    setWithNull.add(new ArrayList<String>(Arrays
-                        .asList(new String[] {
-                                "one",
-                                "two",
-                                null })));
-                    setTransientValue(0, setWithNull);
-                }
-            });
-
-        for (final String scenToken : getParameter().getScenario()) {
-
-
-            final Pattern pattern = Pattern.compile("[a-z]+_\\d(x\\d)*$");
-            final Matcher matcher = pattern.matcher(scenToken);
-            if (matcher.find()) {
-                final Variable currentVar = Variable.valueOf(scenToken);
-
-                source.addTransientCase(
-                    0,
-                    createListOfSet(scenToken),
-                    currentVar);
-            }
-        }
-
-        source.notifyObservers();
+        Assert.assertEquals(
+            "[[false, false], [false, true], [true, false], [true, true]]",
+            result.toString());
 
     }
-
-    private List<List<String>> createListOfSet(final String scenToken)
-    {
-        final String indecesRaw = scenToken
-            .substring(scenToken.indexOf('_') + 1);
-        final String[] indeces = indecesRaw.split("x");
-        final List<List<String>> listOfList = new ArrayList<List<String>>();
-        for (final String index : indeces) {
-            listOfList.add(createList(Integer.parseInt(index)));
-        }
-        return listOfList;
-    }
-
-    /**
-     * Create arbitrary set for testing.
-     *
-     * @param size size of set to create.
-     */
-    List<String> createList(final int size)
-    {
-        final List<String> indexList = new ArrayList<String>();
-        for (int i = 0; i < size; i++) {
-            indexList.add(String.valueOf(i));
-        }
-
-        return indexList;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    protected Object execute()
-    {
-        final List<List<String>> param = getTransientValue(0);
-        try {
-            final int mergeCount = getMockSubject().getMergeCount(param);
-            setResult(String.valueOf(mergeCount));
-        } catch (final IllegalArgumentException iae) {
-            setResult("IAE");
-        }
-        return getResult();
-
-    }
-
-
 }
---
+++ main/src/test/java/junitcast/ListMerger_getCountTest.java
@@ -0,0 +1,173 @@
+/**
+ *
+ */
+package junitcast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Test class for SetMerger.
+ *
+ * <pre>
+ * $Date$
+ * $HeadURL$
+ * </pre>
+ *
+ * @author Royce Remulla
+ */
+public class ListMerger_getCountTest
+        extends
+        AbstractTransientValueTestCase<ListMerger<String>, String, List<List<String>>> {
+
+
+    /** */
+    public enum Variable {
+        /** */
+        list_null, empty_list, list_1x3, list_w_null, list_1x3_w_null,
+
+        /** */
+        list_1x0, list_3x1, list_2x3, list_3x5, list_2x5x3
+    }
+
+    /** @param pParameter Data Transfer Object Parameter in Parameterized test. */
+    public ListMerger_getCountTest(final Parameter<String> pParameter) {
+        super(pParameter);
+    }
+
+    /**
+     * <pre>
+     * Test data generator.
+     * This method is called by the JUnit parameterized test runner and
+     * returns a Collection of Arrays.  For each Array in the Collection,
+     * each array element corresponds to a parameter in the constructor.
+     * </pre>
+     */
+    @Parameters(name = "{0}")
+    public static Collection<Object[]> generateData()
+    {
+        return new ParameterGenerator<String>()
+            .genVarData("junitcast.ListMergerTest");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void setupTargetObject(final List<Object> constructorParams)
+    {
+        new MockitoHelper().setupTargetObject(this, constructorParams);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void prepare()
+    {
+        final ScenarioSource<String> source = new ScenarioSource<String>(this);
+
+        source.addTransientCase(
+            0,
+            new ArrayList<Set<String>>(),
+            Variable.empty_list);
+
+        source.addTransientCase(
+            0,
+            new ArrayList<Set<String>>(),
+            Variable.list_w_null);
+
+        source.addObserver(Variable.list_w_null, new CaseObserver<String>() {
+            @Override
+            public void prepareCase(final String caseRaw)
+            {
+                final List<List<String>> listWithNull = new ArrayList<List<String>>();
+                listWithNull.add(createList(1));
+                listWithNull.add(null);
+                setTransientValue(0, listWithNull);
+            }
+        });
+
+        source.addObserver(
+            Variable.list_1x3_w_null,
+            new CaseObserver<String>() {
+                @Override
+                public void prepareCase(final String caseRaw)
+                {
+                    final List<List<String>> setWithNull = new ArrayList<List<String>>();
+                    setWithNull.add(createList(1));
+                    setWithNull.add(new ArrayList<String>(Arrays
+                        .asList(new String[] {
+                                "one",
+                                "two",
+                                null })));
+                    setTransientValue(0, setWithNull);
+                }
+            });
+
+        for (final String scenToken : getParameter().getScenario()) {
+
+
+            final Pattern pattern = Pattern.compile("[a-z]+_\\d(x\\d)*$");
+            final Matcher matcher = pattern.matcher(scenToken);
+            if (matcher.find()) {
+                final Variable currentVar = Variable.valueOf(scenToken);
+
+                source.addTransientCase(
+                    0,
+                    createListOfSet(scenToken),
+                    currentVar);
+            }
+        }
+
+        source.notifyObservers();
+
+    }
+
+    private List<List<String>> createListOfSet(final String scenToken)
+    {
+        final String indecesRaw = scenToken
+            .substring(scenToken.indexOf('_') + 1);
+        final String[] indeces = indecesRaw.split("x");
+        final List<List<String>> listOfList = new ArrayList<List<String>>();
+        for (final String index : indeces) {
+            listOfList.add(createList(Integer.parseInt(index)));
+        }
+        return listOfList;
+    }
+
+    /**
+     * Create arbitrary set for testing.
+     *
+     * @param size size of set to create.
+     */
+    List<String> createList(final int size)
+    {
+        final List<String> indexList = new ArrayList<String>();
+        for (int i = 0; i < size; i++) {
+            indexList.add(String.valueOf(i));
+        }
+
+        return indexList;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected Object execute()
+    {
+        final List<List<String>> param = getTransientValue(0);
+        try {
+            final int mergeCount = getMockSubject().getMergeCount(param);
+            setResult(String.valueOf(mergeCount));
+        } catch (final IllegalArgumentException iae) {
+            setResult("IAE");
+        }
+        return getResult();
+
+    }
+
+
+}
